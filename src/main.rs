b0eb0bbeb2597f75c5279690889b986c4950e57c
Mark Zhitomirski <marcuzero@gmail.com>
2026-01-05 00:12:33 +0400

    step 2: "<any>: command not found"

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,13 @@
 use std::io::{self, Write};
 
 fn main() {
-    // stage 1 code
+    // Prompt
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    // Capture the user's command in the "command" variable
+    let mut command = String::new();
+    io::stdin().read_line(&mut command).unwrap();
+
+    println!("{}: command not found", command.trim())
 }
