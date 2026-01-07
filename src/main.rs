d5a708a4a0f9fd5a85ea07d7415ff6bee73c1e5a
mz0 <mz0@outlook.com>
2026-01-08 00:43:11 +0400

    step 6: "type"

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ use std::io::{self, Write};
 
 fn main() {
     let cmd_echo = "echo";
+    let cmd_exit = "exit";
+    let cmd_type = "type";
+    let builtins = vec![cmd_echo, cmd_exit, cmd_type];
     loop {
         // Prompt
         print!("$ ");
@@ -11,16 +14,22 @@ fn main() {
         // Capture the user's command in the "command" variable
         io::stdin().read_line(&mut command).unwrap();
         let cmd: &str = command.trim();
-        if cmd == "exit" { break; }
+        if cmd == cmd_exit { break }
         if cmd == cmd_echo {
             echo("");
-            continue;
+            continue
         }
         match cmd.split_once(char::is_whitespace) {
             Some((first_word, remainder)) => {
                 if first_word == cmd_echo {
                     echo(remainder);
-                };
+                    continue
+                }
+                if first_word == cmd_type && builtins.contains(&remainder) {
+                    println!("{} is a shell builtin", remainder)
+                } else {
+                    println!("{}: not found", remainder)
+                }
             }
             None => {
                 println!("{}: command not found", cmd)
