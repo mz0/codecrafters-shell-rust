c316ae2225ef3fefd7ca0cb141927a01d7e8ee65
mz0 <mz0@outlook.com>
2026-01-10 21:44:58 +0400

    tidy up the control logic

--- src/main.rs
+++ src/main.rs
@@ -11,31 +11,31 @@ fn main() {
         print!("$ ");
         io::stdout().flush().unwrap();
         let mut raw_input = String::new();
-        // Capture the user's command
+
+        // Capture the user's cinput
         io::stdin().read_line(&mut raw_input).unwrap();
         let cmd: &str = raw_input.trim();
-        if cmd == cmd_exit { break }
-        if cmd == cmd_echo {
-            echo("");
-            continue
-        }
-        match cmd.split_once(char::is_whitespace) {
-            Some((first_word, remainder)) => {
-                if first_word == cmd_echo {
-                    echo(remainder);
-                    continue
-                }
-                if first_word == cmd_type && builtins.contains(&remainder) {
-                    println!("{} is a shell builtin", remainder)
-                } else {
-                    println!("{}: not found", remainder)
-                }
-            }
-            None => {
-                println!("{}: command not found", cmd)
-            }
-        }
 
+        if cmd == cmd_exit { break }
+
+        let (first_word, remainder) = cmd.split_once(char::is_whitespace)
+            .unwrap_or((cmd, ""));
+
+        if first_word == cmd_echo {
+            echo(remainder)
+        } else if first_word == cmd_type {
+            type_of(remainder, &builtins)
+        } else {
+            println!("{}: command not found", cmd)
+        }
+    }
+}
+
+fn type_of(s: &str, builtins: &[&str]) {
+    if builtins.contains(&s) {
+        println!("{} is a shell builtin", s)
+    } else {
+        println!("{}: not found", s)
     }
 }
 
